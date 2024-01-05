e97ff6b3f3107279c49be6066c3c6502785ce7a1
angie duran <117625258+duranangie@users.noreply.github.com>
2024-01-05 12:58:03 -0500

    feat: :sparkles: create main

---
+++ main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio2;
+
+/**
+ *
+ * @author user
+ */
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        Image image = new proxyImage("angie.png");
+        
+        image.Display();
+    }
+}
